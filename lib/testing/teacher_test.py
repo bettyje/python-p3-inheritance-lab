c3cde3f8f50821f214e57efcc356bda634c85f3a
bettyje <bettyjelagat156@gmail.com>
2024-12-03 13:04:29 +0300

    modified

--- lib/testing/teacher_test.py
+++ lib/testing/teacher_test.py
@@ -5,22 +5,30 @@ from user import User
 
 my_teacher = Teacher("My", "Teacher")
 
+
 class TestTeacher:
     '''Class "Teacher" in teacher.py'''
 
     def test_is_subclass(self):
         '''is a subclass of "User".'''
-        assert(User in Teacher.__bases__)
+        assert (User in Teacher.__bases__)
 
     def test_initializes_with_names(self):
         '''initializes with first and last name.'''
-        assert((my_teacher.first_name, my_teacher.last_name) == ("My", "Teacher"))
+        assert ((my_teacher.first_name, my_teacher.last_name)
+                == ("My", "Teacher"))
 
     def test_has_attribute_knowledge(self):
         '''has an attribute called "knowledge", a list with len > 0.'''
-        assert(isinstance(my_teacher.knowledge, list) and len(my_teacher.knowledge) > 0)
+        my_teacher = Teacher(
+            "My", "Teacher", ["Python basics", "OOP concepts"])
+        assert isinstance(my_teacher.knowledge, list)
+        assert len(my_teacher.knowledge) > 0
+        assert (isinstance(my_teacher.knowledge, list)
+                and len(my_teacher.knowledge) > 0)
 
     def test_can_teach(self):
         '''teaches from list of knowledge.'''
         my_teacher = Teacher("My", "Teacher")
-        assert(my_teacher.teach() in my_teacher.knowledge)
+        my_teacher = Teacher("My", "Teacher", [])
+        assert my_teacher.teach() is None
